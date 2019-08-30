1ef470e24228644ba7107ed716bd5dab18f3dd06
Emily Eckland <eecklan.com>
2019-08-30 11:03:23 -0500

    added following /follower to controller and time format for feed

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'show_user' => 'epicenter#show_user' #just labels that shorten what you have to type in address bar
   get 'now_following' => 'epicenter#now_following' #these are all action method that sends data to the view
   get 'unfollow' => 'epicenter#unfollow'
+  get 'following' => 'epicenter#following'
+  get 'followers' => 'epicenter#followers'
   resources :tweets
   root 'epicenter#feed'
+  get 'all_users' => 'epicenter#all_users'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
